fbf7a0b9e96f314e708eb63dc678cb4d92fbd499
Sander Wieringa <s.wieringa@student.fontys.nl>
2022-01-20 15:01:02 +0100

    initial commit project

---
+++ src/test/java/com/example/demo/ChatWebsocketExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatWebsocketExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
